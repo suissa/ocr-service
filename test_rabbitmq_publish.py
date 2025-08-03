56482fdd86661f19a511daa3a1be8e5ebfd2ab88
suissa <jnascimento@gmail.com>
2025-08-03 02:13:10 -0300

    vao

--- test_rabbitmq_publish.py
+++ test_rabbitmq_publish.py
@@ -4,15 +4,15 @@ import base64
 
 # Ajuste a URI conforme seu ambiente
 rabbitmq_uri = "amqp://127.0.0.1:5672"
-exchange = "ocr.exchange"
-routing_key = "extract_text"
+exchange = "openai.response"
+routing_key = "6666"
 
 # Gera um base64 fake só pra testar (uma string qualquer, não precisa ser imagem real pro teste chegar!)
-base64_teste = base64.b64encode(b'TESTE DE IMAGEM').decode()
+# base64_teste = base64.b64encode(b'TESTE DE IMAGEM').decode()
 
 # Monta o payload do evento conforme esperado pelo seu handler
 payload = {
-    "base64_string": base64_teste,
+    "text": "TESTE DE IMAGEM",
     "number": "5521999999999"  # Manda um número fictício
 }
 
